1f12f4cf7e3675651d38cc9fe8f4a0910044420a
Bilal Meddah <bilal.meddah@epfl.ch>
2024-07-14 13:32:46 +0200

    use node instead

--- src/env.ts
+++
@@ -1,9 +0,0 @@
-export type Env = {
-    web_domains: string;
-    api_domain: string;
-    PGHOST: string;
-    PGPORT: number;
-    PGDATABASE: string;
-    PGUSER: string;
-    PGPASSWORD: string;
-};
